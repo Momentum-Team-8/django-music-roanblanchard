84501e18ac3c94c680c1bf8bdb8d5c5f808d5d88
roanblanchard <roan.g.blanchard@icloud.com>
2021-06-18 16:50:56 -0400

    albums are showing up on the screen

--- albums/views.py
+++ albums/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Album
+from django.utils import timezone
 
 # Create your views here.
 def album_list(request):
-    return render(request, 'albums/post_list.html', {})
+    albums = Album.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'albums/post_list.html', {'albums': albums})
